f56c3f57666b36bdfa487375398fbf2f391fc346
Chebotov Nickolay <spiritkola@hotmail.com>
2023-02-26 02:01:22 +0300

    update: Add frontend

---
+++ web/utilities/blockchains.js
@@ -0,0 +1,13 @@
+export const blockchains = [
+  {
+    group: "finance",
+    groupLabel: "Finance",
+    item: "Concordium",
+    slug: "concordium",
+    apiSlug: "concordium",
+    icon: "/blockchains/concordium.svg",
+    placeholder: "wallet address",
+    coin: "Ͼ",
+    hide: false,
+  },
+];
